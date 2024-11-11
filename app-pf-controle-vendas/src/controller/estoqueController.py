24db12de870f5a1c0e1b2155c9e63382a7de37cf
Carlos Henrique Bonfim da Silva <carlos.bonfim633@hotmail.com>
2024-11-11 20:40:51 -0300

    Versão 2.0

--- app-pf-controle-vendas/src/controller/estoqueController.py
+++ app-pf-controle-vendas/src/controller/estoqueController.py
@@ -1,15 +1,31 @@
-from fastapi import APIRouter
-from src.models.estoqueModel import EstoqueModel 
+from fastapi import APIRouter, HTTPException
+from src.models.estoqueModel import EstoqueModel
 from src.services.estoqueService import EstoqueService
 from src.models.respostasModel import Resposta
 
 app_router = APIRouter(prefix="/estoque")
 
-@app_router.get("/list", status_code=200)
-async def ListarTodos():
+@app_router.get("/listar", status_code=200)
+async def ListarEstoque():
     resposta = await EstoqueService.ListarTodos()
     return Resposta(resposta)
 
-@app_router.post('/criar', status_code=200)   
-async def CriarDados(estoqueModel: EstoqueModel):
-    await EstoqueService.CriarDados(estoqueModel)
+@app_router.post("/adicionar", status_code=200)
+async def AdicionarEstoque(estoqueModel: EstoqueModel):
+    resposta = await EstoqueService.AdicionarEstoque(estoqueModel)
+    return Resposta(resposta)
+
+@app_router.get("/{id}", status_code=200)
+async def Estoque(id):
+    resposta = await EstoqueService.BuscarEstoque(id, 0)
+    return Resposta(resposta)
+
+@app_router.post("/Atualizar", status_code=200)
+async def AtualizarEstoque(produto_id: str, quantidade: int):
+    resposta = await EstoqueService.Vendas(produto_id, quantidade)
+    return Resposta(resposta)
+
+@app_router.post("/vender", status_code=200)
+async def Vendas (produto_id: str, quantidade: int):
+    resposta = await EstoqueService.Vendas(produto_id, quantidade)
+    return Resposta(resposta)
--- app-pf-controle-vendas/src/services/estoqueService.py
+++ app-pf-controle-vendas/src/services/estoqueService.py
@@ -1,57 +1,59 @@
 from src.database.connection import Estoque
 from src.models.estoqueModel import EstoqueModel
 from fastapi import HTTPException
-from datetime import datetime
-from src.models.respostasModel import Resposta
 
 class EstoqueService:
-    async def ListarTodos() -> list:
+    async def ListarTodos():
         try:
             return list(Estoque.find())
         except Exception as error:
-            raise HTTPException(400, detail=error)
-    
-    async def CriarDados(estoqueModel: EstoqueModel):
-        try:
-            estoque = Estoque.count_documents()
-            id = 0
-            if estoque > 1: 
-                id = estoque + 1
+            raise HTTPException(400, detail=str(error))
 
-            novo = {
-                "id": id,
-                "nome": estoqueModel.nome,
-                "sobrenome": estoqueModel.sobrenome,
-                "email": estoqueModel.email,
-                "telefone": estoqueModel.telefone,
-                "datacricao": datetime.now()
+    async def AdicionarEstoque(estoqueModel: EstoqueModel):
+        try:
+            produto_existente = Estoque.find_one({"produto_id": estoqueModel.produto_id})
+
+            if produto_existente:
+                raise HTTPException(status_code=400, detail="Produto já cadastrado no estoque.")
+
+            novo_produto = {
+                "produto_id": estoqueModel.produto_id,
+                "nome_produto": estoqueModel.nome_produto,
+                "quantidade": estoqueModel.quantidade,
+                "preco_unitario": estoqueModel.preco_unitario,
+                "data_criacao": estoqueModel.data_criacao
             }
-            Estoque.insert_one(novo)
+            Estoque.insert_one(novo_produto)
+            return {"message": "Produto adicionado com sucesso."}
+
         except Exception as error:
-            raise HTTPException(400, detail=error)
-            
-    async def BuscarEstoqueId(id):
+            raise HTTPException(400, detail=str(error))
+
+    async def BuscarEstoque(produto_id: str, quantidade: int):
         try:
-            resultado = Estoque.find_one({"id": id}) 
-            resposta = Resposta(resultado)
+            produto = Estoque.find_one({"produto_id": produto_id})
+            if not produto:
+                raise HTTPException(status_code=404, detail="Produto não encontrado.")
 
-            if resposta == None:
-                erro = {
-                "code": "404",
-                "description" : "Não Encontrado!", 
-                "parameter_name": f'{id}'
-                }
-                return  erro
-            
-            return resposta
+            if int(produto['quantidade']) < quantidade:
+                raise HTTPException(status_code=400, detail="Estoque insuficiente.")
+
+            return produto
         except Exception as error:
-            raise HTTPException(400, detail=error)
-        
-
-
-
-
-# nome = "Carlos"
-# idade = "29"
-# email = "carlos@gmail.com"
-# telefone = "999999999"
+            raise HTTPException(400, detail=str(error))
+    
+    async def Vendas (produto_id: str, quantidade: int):
+        try:
+            produto = Estoque.find_one({"produto_id": produto_id})
+            if not produto:
+                raise HTTPException(status_code=404, detail="Produto não encontrado.")
+            if produto['quantidade'] < quantidade:
+                raise HTTPException(status_code=400, detail="Estoque insuficiente.")
+            Estoque.update_one(
+                {"produto_id": produto_id},
+                {"$inc": {"quantidade": -quantidade}}
+                )
+            return {"message": "Venda realizada com sucesso."}
+        except Exception as error:
+            raise HTTPException(400, detail=str(error))
+       
